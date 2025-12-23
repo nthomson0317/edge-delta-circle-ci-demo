5d63c848ac540b11878049f418b5230aae63b077
Nicholas Thomson <nicholasthomson@mac.lan>
2025-12-23 12:01:25 -0500

    Add canary probe job emitting canary vs baseline telemetry

--- app/simulate_5xx_spike.py
+++ app/simulate_5xx_spike.py
@@ -4,7 +4,7 @@ import random
 import os
 import json
 
-EDGE_DELTA_HTTP_URL = os.environ.get("EDGE_DELTA_HTTP_URL")
+EDGE_DELTA_HTTP_URL = os.environ.get("https://6ebf4e82-073c-426b-9f56-f9b1915c6ab1-http-us-west2-cf.aws.edgedelta.com")
 SERVICE_NAME = os.environ.get("SERVICE_NAME", "http-80")
 
 def send_request(status_code):
